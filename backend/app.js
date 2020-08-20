3698d9fb69691bdcf019ccefef929c491955116c
MillanUka <ukamillan@gmail.com>
2020-08-20 17:49:02 +1200

    Add check for production in app.js

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,19 @@ const PORT = process.env.PORT || 5000;
 const buildPath = path.join(__dirname, '..', 'build');
 app.use(express.static(buildPath));
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('frontend/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}
+
+
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`);
-  });
+});
+
