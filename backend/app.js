dee8d8c5f3c4b0bd528959641cbca77c59bad092
MillanUka <ukamillan@gmail.com>
2020-08-20 17:34:51 +1200

    Add server code

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,12 @@
-app.use(express.static(path.join(__dirname, 'build')));
+const path = require('path');
+const express = require('express');
+const app = express();
 
+const PORT = process.env.PORT || 5000;
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+const buildPath = path.join(__dirname, '..', 'build');
+app.use(express.static(buildPath));
+
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
+  });
