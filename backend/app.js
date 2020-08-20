17ec3fe06f8c8724972b3d5bd0de9896bc658c21
MillanUka <ukamillan@gmail.com>
2020-08-20 17:52:56 +1200

    Change the directory

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(express.static(buildPath));
 
 if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
-    app.use(express.static('frontend/build'));
+    app.use(express.static('../frontend/build'));
 
     // Express serve up index.html file if it doesn't recognize route
     const path = require('path');
