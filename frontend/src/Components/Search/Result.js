cd08cd900091272f25a795dd1a0e7a9577b243f9
Jullan Quevedo <jullan.quevedo50@gmail.com>
2020-10-06 19:00:46 +1300

    add card layout

--- frontend/src/Components/Search/Result.js
+++ frontend/src/Components/Search/Result.js
@@ -1,17 +1,22 @@
 import React from "react";
+import "./Result.css";
 export default function Result(props) {
   const { data } = props;
   return (
     <div className={"searchResult"}>
-      <h4>{data.title}</h4>
-      by{" "}
+      <h4>Title </h4>
+      <p>{data.title}</p>
       {data.authors.map((author, key) => {
         return (
           <span key={key}>
+            <h4>Author</h4>
            {author + " "}
           </span>
         );
+
       })}
+      <h4>Published Date</h4>
+      <h4>Evidence Type</h4>
     </div>
   );
 }
